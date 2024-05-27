c13628068d9fd25cf30545a63b669dcfb3c7bfa8
hemmemh <makhmutov_tagir@bk.ru>
2024-05-27 11:16:27 +0300

    new name

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,7 +1,7 @@
-CREATE USER work_time_groups WITH PASSWORD 'work_time_groups' CREATEDB;
-CREATE DATABASE work_time_groups
+CREATE USER work_time_settings WITH PASSWORD 'work_time_settings' CREATEDB;
+CREATE DATABASE work_time_settings
     WITH
-    OWNER = work_time_groups
+    OWNER = work_time_settings
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.utf8'
     LC_CTYPE = 'en_US.utf8'
