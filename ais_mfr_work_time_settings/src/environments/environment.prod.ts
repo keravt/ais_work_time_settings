502c6ed7e6c149bc68cb0d374dc2d28ff352bdcb
hemmemh <makhmutov_tagir@bk.ru>
2024-06-07 15:50:56 +0300

    update staffingTablePath

--- ais_mfr_work_time_settings/src/environments/environment.prod.ts
+++ ais_mfr_work_time_settings/src/environments/environment.prod.ts
@@ -10,4 +10,4 @@ export const keycloakConfigInfo = {
 
 export const mainURL = 'https://api.worktime.k-portal.ru';
 export const authUrl = 'https://api.authorization.k-portal.ru';
-export const staffingTableURL = 'https://test-staffing-table-api.k-portal.ru';
+export const staffingTableURL = 'https://api.staffingtable.k-portal.ru';
