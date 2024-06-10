8cc20c1910fb1119f3c9ddb83e613a1a8e3e0c5d
hemmemh <makhmutov_tagir@bk.ru>
2024-06-10 15:47:20 +0300

    add messagePattertn

--- ais_msrv_work_time_settings/src/main/work-time-settings/work-time-settings.controller.ts
+++ ais_msrv_work_time_settings/src/main/work-time-settings/work-time-settings.controller.ts
@@ -254,5 +254,11 @@ export class WorkTimeSettingsController {
   }
 
 
+
+  @MessagePattern('get-holidays')
+  getHolidaysByUserUids(@Payload() getHolidaysDto: GetHolidaysDto) {
+    return this.workTimeSettingService.getHolidaysByUserUids(getHolidaysDto)
+  }
+  
  
 }
